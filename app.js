a2045be15471f901573153263cf923deae741adc
mitchgams <33948338+mitchgams@users.noreply.github.com>
2020-08-24 17:03:05 -0500

    fixed the grammer in the song

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ for(i = 0; friends.length > i; i++) {
         if(num != 0) {
             num = 100 - j;
             nextNum = num - 1;
-            console.log(num + " lines of code in the file, " + num + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + nextNum +" lines of code in the file")
+            if(num != 1) {
+                console.log(num + " lines of code in the file, " + num + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + nextNum +" lines of code in the file");
+            } else {
+                console.log(num + " line of code in the file, " + num + " line of code; " + friends[i] + " strikes one out, clears it all out, " + nextNum +" lines of code in the file");
+            }
         }
     }
 }
